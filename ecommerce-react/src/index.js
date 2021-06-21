f1a0586eaf11f51d719c03879948e707bbad158e
Daniel Lennertz <danielelennertz@gmail.com>
2021-06-21 14:45:40 -0300

    reuter

--- ecommerce-react/src/index.js
+++ ecommerce-react/src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Cliente from './components/Cliente/Cliente'
 import Produto from './components/Produto/Produto'
 import Categoria from './components/Categoria/Categoria'
-import Funcionario from './components/Funcionario/fu'
+import Funcionario from './components/Funcionario/Funcionario'
 
 ReactDOM.render(
   <BrowserRouter>
