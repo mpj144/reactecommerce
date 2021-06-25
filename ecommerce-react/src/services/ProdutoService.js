8ae7d029cba50f266542ffc66ea3a722217c8eb0
mpj144 <marcelopjrdev@gmail.com>
2021-06-25 02:18:46 -0300

    cadastro de produto com foto

--- ecommerce-react/src/services/ProdutoService.js
+++ ecommerce-react/src/services/ProdutoService.js
@@ -1,23 +1,27 @@
 import api from './api';
 
-export default class ProdutoService{
-    getProduto(){
+export default class ProdutoService {
+    getProduto() {
         return api.get('/produto').then(res => res.data);
     }
 
-    getProdutoById(id){
-        return api.get('/produto/'+id).then(res => res.data);
+    getProdutoById(id) {
+        return api.get('/produto/' + id).then(res => res.data);
     }
 
-    deleteProdutoId(id){
-        return api.delete('/produto/'+id).then(res => res.data)
+    deleteProdutoId(id) {
+        return api.delete('/produto/' + id).then(res => res.data)
     }
 
-    createProduto(newProduto){
+    createProduto(newProduto) {
         return api.post('/produto', newProduto).then(res => res.data);
     }
 
-    updateProduto(newProduto,id){
-        return api.put('/produto/'+id, newProduto).then(res => res.data);
+    createProdutoComFoto(file, newProduto) {
+        return api.post('/produto/comfoto', file, newProduto).then(res => res.data);
+    }
+
+    updateProduto(newProduto, id) {
+        return api.put('/produto/' + id, newProduto).then(res => res.data);
     }
 }
